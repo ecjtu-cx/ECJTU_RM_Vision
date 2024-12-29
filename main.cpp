9695ef8f32e51601de1e97570abf32f2e88a3f09
chengxi <1936175142@qq.com>
2024-12-29 23:10:38 +0800

    2024-12-29 完成测试识别的main函数

--- main.cpp
+++ main.cpp
@@ -1,17 +1,19 @@
 #include "Camera.h"
-
+#include "detector.h"
 #include <opencv2/core.hpp>
 
 int main() {
-    HDURM::HKcam hkcam;
-    hkcam.OpenCam("2BDFA2166410");
-    hkcam.SetParam();
-    cv::Mat img;
-    while(1){
-        hkcam.GetFlame(img);
-        cv::imshow("img", img);
-        cv::waitKey(1);
-    }
-    hkcam.CloseCam();
-    return 0;
+  HDURM::HKcam hkcam;
+  hkcam.OpenCam("2BDFA2166410");
+  hkcam.SetParam();
+  std::unique_ptr<rm_auto_aim::Detector> autoAim;
+  VisionSendData data;
+  cv::Mat img;
+  while (1) {
+    hkcam.GetFlame(img);
+    autoAim->run(img, 0, data);
+    cv::waitKey(1);
+  }
+  hkcam.CloseCam();
+  return 0;
 }
